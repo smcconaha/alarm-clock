1dd8c20714cfa1d27308cd9d6bda596aec358e1f
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-09-14 20:37:31 +0000

    added div class and appended time elements

--- js/main.js
+++ js/main.js
@@ -3,6 +3,30 @@ const month = date.getMonth()+1; //using getXX() methods to retrieve individual 
 const day = date.getDate();
 const year = date.getFullYear();
 const hours = date.getHours()-12;//military time
-const min = date.getMinutes();
+const minute = date.getMinutes();
 const sec = date.getSeconds();
-console.log(hours);
+
+
+const hourSpan = document.createElement('span');
+hourSpan.textContent = hours;
+document.getElementById('container').appendChild(hourSpan);
+
+const minSpan = document.createElement('span');
+minSpan.textContent = minute;
+document.getElementById('container').appendChild(minSpan);
+
+const secSpan = document.createElement('span');
+secSpan.textContent = sec;
+document.getElementById('container').appendChild(secSpan);
+
+const monSpan = document.createElement('span');
+monSpan.textContent = month;
+document.getElementById('container').appendChild(monSpan);
+
+const daySpan = document.createElement('span');
+daySpan.textContent = day;
+document.getElementById('container').appendChild(daySpan);
+
+const yearSpan = document.createElement('span');
+yearSpan.textContent = year;
+document.getElementById('container').appendChild(yearSpan);
