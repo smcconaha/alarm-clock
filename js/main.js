f9e01859593d2e5a7b48d0666acd9eb8d63a66c5
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-09-15 22:43:34 +0000

    Revised JS structure to allow for setInterval method to properly work.  Added alarm button.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,51 @@
 //Defining basic time variables
+function timePull() {
+    const date = new Date();
+    const milHours = date.getHours();
+    const minute = date.getMinutes();
+    const sec = date.getSeconds();
 
+    //Hours military to standard conversion and AM/PM extraction
+    let amPm;
+    let hour;
+    if (milHours < 12) {
+        amPm = "AM";
+        hour = milHours;
+    } else {
+        amPm = "PM";
+        hour = milHours - 12;
+    };
+    let currentTime = `${hour}:${minute}:${sec} ${amPm}`;
+    
+    let timeSpan = document.getElementById('clock');
+    return timeSpan.textContent = currentTime;
+};
+setInterval(timePull, 1000);
+
+/*const alarm = document.getElementById('alarm');
+let contents = alarm.innerHTML;*/
+
+//Alarm
+let setTime = document.getElementById('alarm');
+const btn = document.getElementById('btn');
+function setAlarm (time) {
+    if (setTime.value !== '') {
+        alert('Alarm set');
+    } else {
+        alert('Please set alarm');
+    }
+};
+btn.addEventListener('click',() => {
+    let timeRec = document.createElement('h5');
+    timeRec.textContent = `Alarm set for ${setTime}`;
+    document.body.appendChild(timeRec);
+})
+
+//Month, Day, Year
 const date = new Date();
 const monthInd = date.getMonth();
-const milHours = date.getHours();
 const day = date.getDate();
 const year = date.getFullYear();
-const minute = date.getMinutes();
-const sec = date.getSeconds();
 
 //Month number to string conversion
 let monthArr = [
@@ -27,73 +66,6 @@ for (let i = 0; i < monthArr.length; i++) {
     month = monthArr[monthInd];
 }
 
-//Hours military to standard conversion
-function milConv (hours) {
-    if (hours > 12) {
-        return hours - 12;
-    } else {
-        return hours;
-    }
-};
-
-function amPm (hours) {
-    milHours < 12 ? hours = 'AM' : hours = 'PM';
-    return hours;
-}
-
-const timeSpan = document.createElement('span');
-timeSpan.textContent = `${milConv(milHours)}:${minute}:${sec} ${amPm(milHours)} ${month} ${day} ${year}`;
-document.getElementById('container').appendChild(timeSpan);
-
-
-
-
-/*const hourSpan = document.createElement('span');
-hourSpan.textContent = milConv(milHours);
-document.getElementById('container').appendChild(hourSpan);
-
-const minSpan = document.createElement('span');
-minSpan.textContent = minute;
-document.getElementById('container').appendChild(minSpan);
-
-const secSpan = document.createElement('span');
-secSpan.textContent = sec;
-document.getElementById('container').appendChild(secSpan);
-
-const amPmSpan = document.createElement('span');
-amPmSpan.textContent = amPm(milHours);
-document.getElementById('container').appendChild(amPmSpan);
-
-const monSpan = document.createElement('span');
-monSpan.textContent = month;
-document.getElementById('container').appendChild(monSpan);
-
-const daySpan = document.createElement('span');
-daySpan.textContent = day;
-document.getElementById('container').appendChild(daySpan);
-
-const yearSpan = document.createElement('span');
-yearSpan.textContent = year;
-document.getElementById('container').appendChild(yearSpan);*/
-
-
-
-//Create Alarm
-/*const alarm = document.createElement('input');
-alarm.setAttribute('type', 'time');
-alarm.setAttribute('name', 'alarm');
-alarm.setAttribute('id', 'alarm');
-document.querySelector('label').appendChild(alarm);*/
-
-const setTime = document.getElementById('alarmer');
-const contents = setTime.innerHTML;
-console.log(contents);
-
-/*Test Alarm
-function setAlarm (time) {
-    if (document.getElementById('alarm').value !== '') {
-        alert('Alarm Set');
-    } else {
-        alert('Please set alarm');
-    }
-}; */
+const dateSpan = document.createElement('span');
+dateSpan.textContent = `${month} ${day} ${year}`;
+document.getElementById('container').appendChild(dateSpan);
